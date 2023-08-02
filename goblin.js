279263634f12c75d0d15147810bfcbac6880b7bc
sofianek9 <kayasofiane7@gmail.com>
2023-08-02 19:20:51 +0200

    Mise à jour Goblin

--- goblin.js
+++ goblin.js
@@ -27,13 +27,20 @@ function Musique() {
 // Horloge
 
 function updateClock() {
+  // Récupére l'élément HTML qui affichera l'heure en temps réel
   const clockElement = document.getElementById("clock");
+  // Créé un nouvel objet Date qui représente l'heure
   const now = new Date();
+  //  Récupére l'heure actuelle et l'affiche avec deux chiffres
   const hours = String(now.getHours()).padStart(2, "0");
+  //  Récupére les minutes actuelle et l'affiche avec deux chiffres
   const minutes = String(now.getMinutes()).padStart(2, "0");
+  //  Récupére les secondes actuelle et l'affiche avec deux chiffres
   const seconds = String(now.getSeconds()).padStart(2, "0");
+  // Chaine de caractéres pour afficher les constantes
   const timeString = `${hours}:${minutes}:${seconds}`;
+  // Met à jour le contenu de l'élément HTML avec l'heure au format "hh:mm:ss"
   clockElement.textContent = timeString;
 }
-
+// Met à jour l'heure toutes les secondes
 setInterval(updateClock, 1000);
