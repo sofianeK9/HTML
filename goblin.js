ce75281a999771edf481eb3e25e0b0b8fae923d0
sofianek9 <kayasofiane7@gmail.com>
2023-08-08 19:05:16 +0200

    Mise à jour Goblin

--- goblin.js
+++ goblin.js
@@ -15,11 +15,11 @@ document.addEventListener("DOMContentLoaded", Musique);
 var audio = document.getElementById("background-audio");
 // On utilise un événement écouteur qu'on applique sur audio, l'événement est déclenché lorsque les données de l'audio sont chargées. on créé une fonction musique
 // qui lancera la musique(audio) de facon automatique
-// audio.addEventListener("loadedmetadata", Musique);
+audio.addEventListener("loadedmetadata", Musique);
 
-// function Musique() {
-//   audio.play();
-// }
+function Musique() {
+  audio.play();
+}
 
 // Horloge
 
